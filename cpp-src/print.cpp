cf739a7e3ad0aa5eb5ff57b028118ca67f4c3cce
Moses <moses.l.solomon@gmail.com>
2024-08-29 13:13:00 -0400

    prettier print with row and column names

--- cpp-src/print.cpp
+++ cpp-src/print.cpp
@@ -1,14 +1,17 @@
 #include "print.h"
 
+int rows[] = {8, 7, 6, 5, 4, 3, 2, 1};
+char files[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
 char const *sqToStr[64] = {
     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
-     "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-     "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
-     "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
-     "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
-     "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
-     "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
-     "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"};
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"};
 
 char const *pieceToStr[7] = {
     "PAWN", "BISHOP", "KNIGHT", "ROOK", "QUEEN", "KING", "empty"};
@@ -42,9 +45,12 @@ void printSquare()
 // a1 - a8
 void printBitString(uint64_t bitboard)
 {
+    printf("\n");
     int start;
+    int counter = 0;
     for (int i = 8; i > 0; i--)
     {
+        printf("%d   ", rows[counter++]);
 
         start = (i * 8) - 8; // 56
 
@@ -54,18 +60,28 @@ void printBitString(uint64_t bitboard)
         }
         printf("\n");
     }
-    printf("\n");
+
+    printf("\n    ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%c ", files[i]);
+    }
+    printf("\n\n");
     return;
 }
 
+
 void printBitString(uint64_t bitboard, int square)
 {
+    printf("\n");
     int start;
+    int counter = 0;
+    //
     for (int i = 8; i > 0; i--)
     {
+        printf("%d   ", rows[counter++]);
 
         start = (i * 8) - 8; // 56
-
         for (int pos = start; pos < start + 8; pos++)
         {
             if (pos == square)
@@ -79,7 +95,12 @@ void printBitString(uint64_t bitboard, int square)
         }
         printf("\n");
     }
-    printf("\n");
+    printf("\n    ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%c ", files[i]);
+    }
+    printf("\n\n");
     return;
 }
 
