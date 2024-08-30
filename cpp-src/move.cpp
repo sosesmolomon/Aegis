6f229276833bebe290bf2b2e2ed4bf2325f4ffc2
Moses <moses.l.solomon@gmail.com>
2024-08-30 13:57:35 -0400

    undoMove works for castling and enpassant

--- cpp-src/move.cpp
+++ cpp-src/move.cpp
@@ -80,17 +80,78 @@ void makeMove(CBoard *b, moveStruct m, MoveList *game)
 
         // turn off home square for the piece moved.
         b->pieceHomes ^= (1ULL << m.from);
-
     }
-        game->add(m);
+    game->add(m);
 }
 
 // quiet move, no capture...
 void undoMove(CBoard *b, moveStruct m, MoveList *game)
 {
-    b->setSq(m.pT, m.pC, m.from);
-    b->setSq(empty, m.pC, m.to);
+    if (m.isCastlingLong || m.isCastlingLong)
+    {
+        if (m.isCastlingShort)
+        {
+            // white
+            if (m.pC)
+            {
+                b->setSq(KING, WHITE, e1);
+                b->setSq(ROOK, WHITE, h1);
 
+                b->setSq(empty, WHITE, g1);
+                b->setSq(empty, WHITE, f1);
+            }
+            // black
+            else
+            {
+                b->setSq(KING, WHITE, e8);
+                b->setSq(ROOK, WHITE, h8);
+
+                b->setSq(empty, WHITE, g8);
+                b->setSq(empty, WHITE, f8);
+            }
+        }
+        else
+        {
+            // white
+            if (m.pC)
+            {
+                b->setSq(KING, WHITE, e1);
+                b->setSq(ROOK, WHITE, a1);
+
+                b->setSq(empty, WHITE, c1);
+                b->setSq(empty, WHITE, d1);
+            }
+            // black
+            else
+            {
+                b->setSq(KING, WHITE, e8);
+                b->setSq(ROOK, WHITE, a8);
+
+                b->setSq(empty, WHITE, c8);
+                b->setSq(empty, WHITE, d8);
+            }
+        }
+    }
+    else
+    {
+        assert((b->coloredBB[m.pC] & 1ULL << m.to) == 0 && "Capture same color");
+
+        // is capture?
+        if (m.isCapture)
+        {
+            int opp_piece_sq = m.to;
+            if (m.isEnPassant)
+            {
+                opp_piece_sq = game->at(game->size() - 1).to;
+            }
+            // turn the pawn back on before en passant capture
+            b->setSq(PAWN, oppColor(m.pC), opp_piece_sq);
+
+        }
+
+        b->setSq(m.pT, m.pC, m.from);
+        b->setSq(empty, m.pC, m.to);
+    }
     // find identify the right move to unmove?
-    // game->remove();
+    game->remove(game->size()-1); // remove just the end of the list?
 }
