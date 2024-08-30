d4987dad168597f5abb925a10c4a99defe4d9a88
Moses <moses.l.solomon@gmail.com>
2024-08-30 14:15:38 -0400

    most up to date! all movement, castling, capturing, and undoing should be working... i should probably figure out how to test all of that... and then refactor the spagetti

--- cpp-src/main.cpp
+++ cpp-src/main.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <stdio.h>
+#include <iostream>
 #include "print.h"
 #include "CBoard.h"
 // #include "init.h"
@@ -11,31 +12,57 @@
 // a1 - a8, b1, h1 - h8
 //  0 - 7,  8,  56 - 63
 
-
 int main()
 {
     CBoard board = CBoard();
     CBoard *b = &board;
 
-    b->initCBoard();
+    // b->initCBoard();
+    b->initTestBoard();
     initMagic(b);
 
-    // move list for: 1. the game's current string of moves
-    //                2. curren position: all of the possible moves to evalute
-                            // or is this:
-                                // attackBB[piece] -- where only legal moves are 1
-    
     MoveList game_ml = MoveList();
-    // ml.curr_idx = 0;
 
     MoveList possible_moves = MoveList();
 
-    // b->setSq(ROOK, WHITE, c6);
-    // b->setSq(BISHOP, WHITE, c3);
-    // b->setSq(PAWN, WHITE, g7);
-    
-    b->genAllLegalMoves(&possible_moves, WHITE);
+    game_ml.print();
+
+    // b->setSq(PAWN, WHITE, a5);
+    // b->setSq(KNIGHT, BLACK, h3);
+
+    // gen moves --> populates legalAttacks --> informs squareIsAttacked() --> know castling options
+    b->genAllLegalMoves(&possible_moves, &game_ml, WHITE);
+    b->genAllLegalMoves(&possible_moves, &game_ml, BLACK);
+    possible_moves.clear();
+    // --------------------------------------------------
+
+    // reset white's attacked squares by regenerating moves
+    b->legalAttackedSquares[WHITE].getBB() = 0ULL;
+    b->genAllLegalMoves(&possible_moves, &game_ml, WHITE);
+
+    b->legalAttackedSquares[BLACK].getBB() = 0ULL;
+    b->genAllLegalMoves(&possible_moves, &game_ml, BLACK);
+
     possible_moves.print();
-    printf("count = %d\n", possible_moves.size());
+
+    makeMove(b, possible_moves.at(10), &game_ml);
+
+    possible_moves.clear();
+    b->legalAttackedSquares[WHITE].getBB() = 0ULL;
+    b->genAllLegalMoves(&possible_moves, &game_ml, WHITE);
+
+    b->legalAttackedSquares[BLACK].getBB() = 0ULL;
+    b->genAllLegalMoves(&possible_moves, &game_ml, BLACK);
+    possible_moves.print();
+
+    makeMove(b, possible_moves.at(1), &game_ml);
+    printBoard(b, b->fullBoard());
+
+    possible_moves.print(1);
+
+
+    undoMove(b, possible_moves.at(1), &game_ml);
+    printBoard(b, b->fullBoard());
+
     return 0;
 }
