ba3fb4a2f5f10a431eeb958858e48d454a207e26
Moses <moses.l.solomon@gmail.com>
2024-09-13 14:19:42 -0400

    added isInCheck method. not perfect because you need to recalculate attacks before checking

--- cpp-src/utils.h
+++ cpp-src/utils.h
@@ -23,12 +23,16 @@ bool canEnPassant(MoveList *game, int sq, int target, int player_color);
 
 // move to CBoard?
 bool squareIsAttacked(CBoard *b, int sq, int color);
+bool pieceIsDefended(CBoard *b, int sq, int color);
 bool pawnOnHome(CBoard *b, int sq, int color);
 bool isEmptySquare(CBoard* b, int square);
 int identifyPieceType(CBoard *b, int sq);
 int identifyPieceColor(CBoard *b, int sq);
 bool isOpposingPiece(CBoard *b, int start, int target, int opp_color);
 bool noFriendlyFire(CBoard *b, int start, int target, int player_color);
+void updateMoveLists(CBoard *b, MoveList *possible_moves, MoveList *game, int color, MoveList *legal_moves);
+// bool isInCheck(CBoard *b, int color);
+
 
 
 #endif
