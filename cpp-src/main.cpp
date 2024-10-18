db8fe6a74fc94f3251205ac0a3f903ca0d0729fc
Moses <moses.l.solomon@gmail.com>
2024-10-17 23:43:08 -0400

    working on a bunch of test cases

--- cpp-src/main.cpp
+++ cpp-src/main.cpp
@@ -79,13 +79,12 @@ void makeRandomMoves(CBoard *b, MoveList *possible_moves, MoveList *game, int n)
             exit(1);
         }
 
-    
         printf("size of legals = %lu\n", legals->size());
         printf("evaluating for %s\n", colorToStr[b->player]);
         r = bestMoveIndex(b, legals, game, b->player);
         printf("best move at %d\n", r);
         m = legals->at(r);
-        
+
         makeDefinedMove(b, m, legals, game);
 
         printf("\n  %s %s | %s to %s", colorToStr[m.pC], pieceToStr[m.pT], sqToStr[m.from], sqToStr[m.to]);
@@ -116,56 +115,36 @@ int main()
     CBoard board = CBoard();
     CBoard *b = &board;
 
-    // test
-    // b->loadFEN("4k2r/6r1/8/8/8/8/3R4/R3K3 w Qk - 0 1");
-
-    // Normal board setup
-    // b->loadFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-
-    // En Passant setup
-    // b->loadFEN("8/1p6/8/2P5/8/8/8/8 w - b6 0 1");
-
-    // Legal castling setup
-    // b->loadFEN("r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1");
-
-    // Illegal castling through check setup
-    b->loadFEN("5rk1/8/8/8/8/8/8/4K2R w K - 0 1");
-    exit(1);
-
-    // Stalemate setup
-    b->loadFEN("7k/5Q2/6Q1/8/8/8/8/7K b - - 0 1");
-
-    // Checkmate setup
-    b->loadFEN("7k/5Q2/8/8/8/8/8/7K b - - 0 1");
-    
-    exit(1);
-
-    b->initCBoard();
+    b->generatePiecePossibleMoves();
     initMagic(b);
 
-
-    printBitString(0b000100000000000000000000000000000000000000000000000000000001000);
-    printBitString(b->pieceBB[QUEEN]);
-    exit(1);
 
     MoveList *game = new MoveList();
     MoveList *possible_moves = new MoveList();
     MoveList *legal_moves_W = new MoveList();
-    MoveList *legal_moves_B = new MoveList();
+    MoveList *legal_moves_B = new MoveList(); 
 
+    b->loadFEN("4r3/8/8/3pP3/8/r7/4K3/k2r1r2 w - d6 0 1", game);
+    game->print();
+
+ 
     // b->fillAttackBBs(game, UINT64_MAX ^ b->coloredBB[BLACK], BLACK);
     // for (int i = 0; i < 6; i++) {
     //     printf("%s:\n", pieceToStr[i]);
     //     printBitString(b->pieceAttacks[BLACK][i]);
     // }
 
+    printBoard(b, b->fullBoard());
+
+
     b->genAllMoves(possible_moves, game, b->player);
-    printf("--------------\n");
+    printf("------------------ %s MOVES -------------------\n", colorToStr[b->player]);
     b->verifyLegalMoves(possible_moves, game, b->player, legal_moves_W);
     legal_moves_W->print();
     legal_moves_W->sort();
 
-    b->player^=WHITE;
+
+    b->player ^= WHITE;
 
     return 0;
 }
